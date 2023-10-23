eb8bf9acf589b288d33f40168e9a6d675509b567
samuckqadev <samuel.tomimatu@bandtec.com.br>
2023-10-23 12:25:33 -0300

    Finding datas through pipe and filter with tap

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -2,22 +2,21 @@ import { CoursesService } from './../services/courses.service';
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
+  courses: Observable<Course[]>;
 
-
-  courses: Course[] = [];
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService) {
     this.courses = this.coursesService.list();
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,22 +1,21 @@
+import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
-import { Course } from '../model/course';
 
 import { HttpClient } from '@angular/common/http';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursesService {
-  
+  private readonly API = '/assets/courses.json';
+
   constructor(private httpClient: HttpClient) {}
 
-  list(): Course[] {
-    return [
-      {
-        _id: '12',
-        name: 'teste',
-        category: 'development',
-      },
-    ];
+  list() {
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap((course) => console.log(course))
+    );
   }
 }
